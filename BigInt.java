225d723b1f1777ea3a888b64c373061ea34584d6
Ronald Santos <36488171+santosr795@users.noreply.github.com>
2019-01-17 21:54:11 -0500

    Update BigInt.java

--- BigInt.java
+++ BigInt.java
@@ -1,4 +1,50 @@
 
 public class BigInt {
-
+	
+	private int bigInt; 
+	private boolean positive = false; 
+	public static void main(String args []) {
+		BigInt b1; 
+		b1 = new BigInt(" ss");
+		b1.toString();
+	}
+	BigInt(){
+		set("0"); 
+	}
+	BigInt(String integer){
+		set(integer); 
+	}
+	private void set(String aBigInt) {
+		try {
+			if(aBigInt.substring(0,1).equals("-")) {
+				positive = true; 
+				this.bigInt = Integer.parseInt(aBigInt.substring(1,aBigInt.length())); 
+				System.out.println("Testing set 1 " + this.bigInt);
+			}
+			else if (aBigInt.substring(0,1).equals("+")) {
+				this.bigInt = Integer.parseInt(aBigInt.substring(1,aBigInt.length()));
+				System.out.println("Testing set 2 " + this.bigInt);
+			}
+			else {
+		
+				this.bigInt = Integer.parseInt(aBigInt); 
+				System.out.println("Testing set 3 " + this.bigInt);
+			}
+		}
+		catch(Exception e) {
+			System.out.println(aBigInt+" was written in the wrong format.");
+			System.exit(0);
+		}
+	}
+	public String toString() {
+		if(positive == true) {
+			return "-"+ String.valueOf(this.bigInt); 
+		}
+		return String.valueOf(this.bigInt); 
+	}
+	public BigInt add(BigInt anInteger) {
+		
+		return null; 
+		}
+	
 }
