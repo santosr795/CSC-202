96558876d4dff6a49355ad7b452906f15ea5f459
Ronald Santos <36488171+santosr795@users.noreply.github.com>
2019-01-27 22:40:39 -0500

    Update BigInt.java

--- BigInt.java
+++ BigInt.java
@@ -1,46 +1,153 @@
-
+import java.util.*;
 public class BigInt {
-	
-	private int bigInt; 
-	private boolean positive = false; 
+	private ArrayList <String> bigArray = new ArrayList<String>(); 
+	private String bigInt; 
+	private int arrayIndex = 0; 
+	private Boolean positive;
 	public static void main(String args []) {
-		BigInt b1; 
-		b1 = new BigInt(" ss");
-		b1.toString();
+		//BigInt b1 = new BigInt("1000000");
+		//BigInt b1 = new BigInt("+0");
+		//BigInt b1 = new BigInt("-0");
+		//BigInt b1 = new BigInt("+");// error
+		//BigInt b1 = new BigInt("-");// error
+		BigInt b1 = new BigInt(	"-44444444445555555555666666666677777777770000000000");
+		System.out.println("b1 it " +b1);
 	}
-	BigInt(){
-		set("0"); 
-	}
-	BigInt(String integer){
-		set(integer); 
-	}
-	private void set(String aBigInt) {
-		try {
-			if(aBigInt.substring(0,1).equals("-")) {
-				positive = true; 
-				this.bigInt = Integer.parseInt(aBigInt.substring(1,aBigInt.length())); 
-				System.out.println("Testing set 1 " + this.bigInt);
+	BigInt(String stringInteger){
+		char location ; 
+		String 	aSubstring =null ;
+		for(int index = 0; index < stringInteger.length(); index++) {
+			location = stringInteger.charAt(index);
+			if (location == 43 && stringInteger.substring(0, 1).equals("+")) {
+				if(stringInteger.length() ==1 ) {
+					throw new StringInputErrorException(stringInteger + " its not a valid input" );
+				}
+				positive = new Boolean(true); 
+				aSubstring = setAndRemoveSignIfItIsThere(stringInteger); 
+				System.out.println("Inside the + if Stateme aSubString value its equalas "+aSubstring);
+				for(index = 0; index < aSubstring.length(); index++) {
+					System.out.println(location+" its a valid input");
+					location = aSubstring.charAt(index);
+					System.out.println("testing inside + if Statement location is equals " + location);
+					if (location < 48 || location > 57) {
+						throw new StringInputErrorException(stringInteger + " its not a valid input" );
+					}
+				}
+				//bigArray.add(arrayIndex, setAndRemoveSignIfItIsThere(stringInteger)); 
+				//arrayIndex++; 
 			}
-			else if (aBigInt.substring(0,1).equals("+")) {
-				this.bigInt = Integer.parseInt(aBigInt.substring(1,aBigInt.length()));
-				System.out.println("Testing set 2 " + this.bigInt);
+			else if (location == 45 && stringInteger.substring(0, 1).equals("-")) {
+				positive = new Boolean(false); 
+				if(stringInteger.length() == 1) {
+					throw new StringInputErrorException(stringInteger + " its not a valid input" );
+				}
+				System.out.println(location+" its a valid input");
+				aSubstring = setAndRemoveSignIfItIsThere(stringInteger); 
+				for(index = 0; index < aSubstring.length(); index++) {
+					System.out.println(location+" its a valid input");
+					location = aSubstring.charAt(index);
+					if (location < 48 || location > 57) {
+						throw new StringInputErrorException(stringInteger + " its not a valid input" );
+					}
+				}
+				//bigArray.add(arrayIndex, setAndRemoveSignIfItIsThere(stringInteger)); 
+				//arrayIndex++; 
 			}
-			else {
+			else if (location < 48 || location > 57) {
+				throw new StringInputErrorException(stringInteger + " its not a valid input" );
+			}
+			
+		}
+		this.bigInt = stringInteger; 
+		if(aSubstring !=null) {
+			this.bigInt= aSubstring;
+		}
 		
-				this.bigInt = Integer.parseInt(aBigInt); 
-				System.out.println("Testing set 3 " + this.bigInt);
+		bigArray.add(arrayIndex, this.bigInt); 
+		arrayIndex++; 
+		System.out.println("it should End here " + this.bigInt);
+		//System.exit(0);
+	}
+//	private void set(String aBigInt) {
+//		try {
+//			if(aBigInt.substring(0,1).equals("-")) {
+//				positive = false; 
+//				aBigInt = setAndRemoveSignIfItIsThere(aBigInt);  
+//				System.out.println("Testing set one " + this.bigInt);
+//			}
+//			else if (aBigInt.substring(0,1).equals("+")) {
+//				aBigInt = setAndRemoveSignIfItIsThere(aBigInt);  
+//				System.out.println("Testing set two " + this.bigInt);
+//			}
+//			else {
+//		
+//				this.bigInt = Integer.parseInt(aBigInt); 
+//				System.out.println("Testing set three " + this.bigInt);
+//			}
+//		}
+//		catch(Exception e) {
+//			System.out.println(aBigInt+" was written in the wrong format.");
+//			System.exit(0);
+//		}
+//	}
+	BigInt(ArrayList<String> arrayString){
+		String  newString= null; 
+		BigInt b1; 
+		for(int index = 0; index < arrayString.size(); index++) {
+			newString  = arrayString.get(index); 
+			b1 = new BigInt(newString);
+		}
+		
+	}
+	BigInt(BigInt integer){
+		
+		BigInt b1 = new BigInt(integer.toString()); 
+	}
+	BigInt(int Integer){
+		new BigInt(String.valueOf(Integer));
+		
+		
+	}
+	private  String setAndRemoveSignIfItIsThere(String number) {
+		int integerNum;
+			if(number.substring(0,1).equals("+")) {
+				System.out.println("Test one SetAndRemove method return value equals " + number.substring(1,number.length()));
+				return number.substring(1,number.length()); 
+				//bigArray.add( number.substring(1,number.length())); 
 			}
-		}
-		catch(Exception e) {
-			System.out.println(aBigInt+" was written in the wrong format.");
-			System.exit(0);
-		}
+			else if (number.substring(0,1).equals("-")) {
+				System.out.println("Test two SetAndRemove method return value equals " + number.substring(1,number.length()));
+				return number.substring(1,number.length());
+				//thnumber.substring(1,number.length()); 
+			}
+			try {
+				if (positive.booleanValue()==false) {
+				
+				 return "-" + number;
+			 }
+			 else if (positive.booleanValue()==true) {
+					return "+" + number; 
+				}
+			}
+			catch(Exception e) {
+				try {			
+					if (positive.booleanValue()==true) {
+					return "+" + number; 
+					}
+				}	
+					catch(Exception r) {
+						return number; 
+					}
+			
+			}
+			System.out.println("Test final SetAndRemove method default return value equals " + number.substring(1,number.length()));
+				return number; 
 	}
 	public String toString() {
-		if(positive == true) {
-			return "-"+ String.valueOf(this.bigInt); 
+		if(!positive.equals("null")){
+			this.bigInt = setAndRemoveSignIfItIsThere(this.bigInt);
 		}
-		return String.valueOf(this.bigInt); 
+		return this.bigInt; 
 	}
 	public BigInt add(BigInt anInteger) {
 		
