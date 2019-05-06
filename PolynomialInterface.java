18ca1e0931ee2b72cddedd0a5421a1ad0b69e350
Ronald Santos <36488171+santosr795@users.noreply.github.com>
2019-05-06 11:11:15 -0400

    Create PolynomialInterface.java

---
+++ PolynomialInterface.java
@@ -0,0 +1,13 @@
+
+public interface PolynomialInterface  {
+
+	PolynomialInterface add(PolynomialInterface other);
+	PolynomialInterface subtract(PolynomialInterface other);
+	void readPolynomial();
+		
+	
+	String toString();
+	
+
+
+}
